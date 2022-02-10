91c0f3d7ea2c352b8213321470f523cde21d77b6
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-10 23:38:37 +0300

    add code to Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3 "Даны два числа, показать большее и меньшее число"
+
+/*int a = new Random().Next(1,11);
+int b = new Random().Next(1,11);
+
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine();*/
+
+System.Console.WriteLine("Введите первое число:");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne > numberTwo) Console.WriteLine($"Число {numberOne} больше, чем {numberTwo}");
+if (numberOne < numberTwo) Console.WriteLine($"Число {numberTwo} больше, чем {numberOne}");
+
+if (numberOne == numberTwo) Console.WriteLine($"Числа {numberOne} и {numberTwo} равны");
