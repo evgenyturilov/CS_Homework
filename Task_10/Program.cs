73a5269cfc493d2d5efa074c4748295360303aed
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-11 01:38:51 +0300

    started to write code in Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 10: "Показать последнюю цифру трехзначного числа"
+
+System.Console.WriteLine("Введите трехзначное число:");
+string N = Console.ReadLine();
+
+System.Console.WriteLine(N);
