b18cf57c1165d856deea6739798f8348d892a4b0
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-13 22:47:34 +0300

    add code in Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число из отрезка [10, 99]. Требуется показать наибольшую цифру данного числа.
+
+int number = new Random().Next(10, 100);
+
+string strNumber = Convert.ToString(number);
+
+if (strNumber[0] > strNumber[1]) System.Console.WriteLine($"Наибольшая цифра в числе {number} - {strNumber[0]}");
+else System.Console.WriteLine($"Наибольшая цифра в числе {number} - {strNumber[1]}");
