57b814834db9ee988d627e318c427b06c98ca570
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-13 22:56:07 +0300

    add code to Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трехзначного числа
+
+
+System.Console.WriteLine("Введите трехзначное число:");
+string number = Console.ReadLine();
+
+number = number.Remove(1, 1);
+
+System.Console.WriteLine(number);
