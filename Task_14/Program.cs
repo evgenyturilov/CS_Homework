0a7c3fd0638cdcad7ae028bbb408bc2a06a2b383
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-13 23:13:35 +0300

    add code to Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить кратно ли число заданному, если нет вывести остаток
+
+System.Console.WriteLine("Введите число:");
+double numberOne = Convert.ToDouble(Console.ReadLine());
+
+double numberTwo = new Random().Next(1,10);
+System.Console.WriteLine($"Заданное число: {numberTwo}");
+
+//int numberOne = 45;
+//int numberTwo = 5;
+
+double restOfDivision = numberOne%numberTwo;
+
+if (restOfDivision == 0) System.Console.WriteLine("Ваше число кратно заданному!");
+else System.Console.WriteLine($"Остаток от деления вашего числа на заданное {restOfDivision}");
+
