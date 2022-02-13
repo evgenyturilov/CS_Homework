97963a5eb9021b9d9faee17e982e8b89b2b2b2e6
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-13 22:26:01 +0300

    add Task_11 and code

---
+++ Task_11/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трехзначного числа
+
+
+System.Console.WriteLine("Введите трехзначное число:");
+string number = Console.ReadLine();
+
+System.Console.WriteLine($"Вторая цифра трехзначного числа: {number[1]}");
