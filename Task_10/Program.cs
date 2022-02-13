ef987cab58152f2588236ffc22fd398bad581537
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-13 22:32:35 +0300

    additional code to Task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,7 +1,14 @@
-﻿// Задача 10: "Показать последнюю цифру трехзначного числа"
+﻿// Задача 10: "Показать последнюю цифру трехзначного числа"/Любого числа
 
-System.Console.WriteLine("Введите трехзначное число:");
+/*System.Console.WriteLine("Введите трехзначное число:");
 string number = Console.ReadLine();
 
 
-System.Console.WriteLine($"Последняя цифра вашего числа: {number[2]}");
+System.Console.WriteLine($"Последняя цифра вашего числа: {number[2]}");*/
+
+System.Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+
+int numberCipher = number.Length;
+
+System.Console.WriteLine($"Последняя цифра в вашем числе: {number[numberCipher - 1]}");
