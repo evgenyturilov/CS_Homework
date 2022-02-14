698c9c63208283b9827b30b367c457bc4c087395
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-14 23:39:14 +0300

    add code to Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int numberA = 5;
+int numberB = 3;
+int result = 1;
+
+for (int i = 0; i < numberB; i++)
+{
+    result = result*numberA;
+}
+System.Console.WriteLine(result);
