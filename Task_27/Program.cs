f25dbcfc765981c1dde9d8e4186ebf2934620d23
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-14 23:56:17 +0300

    add code to Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить количество цифр в числе
+
+/*Console.WriteLine("Введите число:");
+string A = Console.ReadLine();
+
+int numberOfDigits = A.Length;
+
+System.Console.WriteLine(numberOfDigits);*/
+
+int n = 123;
+int n1;
+int count = 0;
+n1 = n;
+
+while(n != 0)
+{
+    n = n/10;
+    count++;
+}
+System.Console.WriteLine("{0,8} {1,8}",n1, count);
