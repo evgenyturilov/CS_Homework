577c18237c57c77445a088b5991e0c1b7c818800
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 13:24:05 +0300

    add code to Task_16

---
+++ Task_16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число, обозначающее день недели. Выяснить является ли номер дня недели выходным.
+
+Console.WriteLine("Введите номер дня недели:");
+int dayNumberInput = Convert.ToInt32(Console.ReadLine());
+
+string[] daysOfWeek = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+
+if (daysOfWeek[dayNumberInput - 1] == "воскресенье" && daysOfWeek[dayNumberInput - 1] == daysOfWeek[6])
+{
+    System.Console.WriteLine($"Выбранный день недели - {daysOfWeek[dayNumberInput - 1]}, {daysOfWeek[dayNumberInput - 1]} - выходной!");
+}
+else
+{
+    System.Console.WriteLine($"Выбранный день недели - {daysOfWeek[dayNumberInput - 1]}, {daysOfWeek[dayNumberInput - 1]} - рабочий день.");
+}
