70268ea256342a428c1e1b970a5cbe085ccf9e70
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 14:01:12 +0300

    add code to Task_32

---
+++ Task_32/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8-ми элементов, заполненный нулями и единицами и вывести их на экран
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,2);
+    System.Console.WriteLine("{0,8}", array[i]);
+}
+
+
