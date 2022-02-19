dd8ff8385a59b7a5166b10efa8e9cfed75b7be34
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 14:17:45 +0300

    add code to Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Подсчитать сумму цифр в числе
+
+/*
+//Решение №1
+
+System.Console.WriteLine("Введите число");
+string yourNumber = Console.ReadLine();
+
+System.Console.WriteLine($"В вашем числе {yourNumber.Length} цифр.");
+*/
+
+// Решение №2
+
+double number = new Random().Next(1,100000000);
+Console.WriteLine(number);
+
+string a = Convert.ToString(number);
+
+Console.WriteLine($"В числе {number} {a.Length} цифр.");
