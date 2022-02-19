f2f2e345f8bb87b00d9858d39c3082cf922f1ef6
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 16:25:46 +0300

    add code to Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+int multNumber = 1;
+int arrayNumber = 1;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = arrayNumber;
+    arrayNumber++;
+    multNumber = multNumber*array[i];
+}
+
+System.Console.WriteLine($"Произведение чисел от 1 до {number} равняется {multNumber}");
