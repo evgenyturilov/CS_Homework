30d01dd3451e6864ce67b9a73335d3e9d71f8329
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 15:33:45 +0300

    add code to Task_15_2

---
+++ Task_15_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23.
+
+System.Console.WriteLine("Введите число:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно ни 7 и ни 23");
+}
