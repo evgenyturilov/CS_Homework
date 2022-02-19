4472e88cbe9598afbf25f6a62aee63c6461c2dbd
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 23:07:57 +0300

    add code to Task_15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,11 +1,39 @@
 ﻿// Найти третью цифру числа, или сообщить, что ее нет
 
-/*Console.WriteLine("Введите число:");
+Console.WriteLine("Введите число:");
+string number =  Console.ReadLine();
+int lenght = number.Length;
 
-string number = Console.ReadLine();
 
-int i;
+if (lenght >= 3) 
+{
+    var thirdNumber = number[2];
+    Console.WriteLine($"Третья цифра в числе {number} - {thirdNumber}");
+}
+else Console.WriteLine("В числе меньше трех знаков");
 
-if ()*/
+
+/*
+int n = Convert.ToInt32(number);
+int n1 = Convert.ToInt32(number);
+int count = 0;
+
+while (n != 0)
+{
+    n = n/10;
+    count++;
+    if (count == (lenght - temp))
+    {
+        n1 = n%10;
+    }
+}
+System.Console.WriteLine(count);
+if (count >= 3)
+{
+    System.Console.WriteLine($"Третья цифра в числе {number} - {n1}");
+}
+else System.Console.WriteLine("В числе меньше трех знаков");
+*/
+
 
 
