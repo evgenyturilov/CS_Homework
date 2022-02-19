2b8ab7f71f2ff833a669015501db74325d95f05e
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 13:54:45 +0300

    add code to Task_31

---
+++ Task_31/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8-ми элементов и вывести их на экран
+
+int[] array = {31, 53,25 ,76 ,28 ,29 ,36, 79};
+
+for(int i = 0; i < array.Length; i++)
+{
+    System.Console.WriteLine("{0, 8}", array[i]);
+}
