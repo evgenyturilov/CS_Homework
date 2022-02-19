4dd95442e10fc663ad7a4736b1ee7a06fe2a0cf7
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-19 13:35:11 +0300

    add code to Task_17

---
+++ Task_17/Program.cs
@@ -0,0 +1,25 @@
+﻿// По двум заданным числам проверить является ли одно квадратом другого
+
+Console.WriteLine("Введите первое число:");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+bool SquareOrNot(int arg1, int arg2)
+{
+    if (arg2*arg2 == arg1) return true;
+    else return false;
+}
+
+if (SquareOrNot(numberOne, numberTwo) == true) 
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else Console.WriteLine("Первое число не является квадратом второго.");
+
+if (SquareOrNot(numberTwo, numberOne) == true)
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}
+else Console.WriteLine("Второе число не является квадратом первого");
