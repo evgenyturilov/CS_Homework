06ea0705bf634056488c59043d00d7edddf1c6ae
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-21 13:15:44 +0300

    add code to Task_21

---
+++ Task_21/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверить пятизначное число на полиндром
+
+System.Console.WriteLine("Ввведите пятизначное число:");
+string number = Console.ReadLine();
+
+int len = number.Length;
+
+if (number[0] == number[len - 1] && number[1] == number[len - 2])
+{
+    System.Console.WriteLine("Ваше число полиндром");
+}
+else System.Console.WriteLine("Вы ввели обычное число.");
