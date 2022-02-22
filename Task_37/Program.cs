b0eb98a0266948d6b388f71c042221d44aba379c
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-22 09:42:06 +0300

    add code to Task_37

---
+++ Task_37/Program.cs
@@ -0,0 +1,33 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99].
+
+int[] arr = new int[123];
+
+void FillArray(int[] arrray)
+{
+    for (int i = 0; i < arrray.Length; i++)
+    {
+        arrray[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int NumOfElements(int[] array, int n1, int n2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= n1 && array[i] <= n2) count = count + 1;
+    }
+    return count;
+}
+
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine($"В данном массиве {NumOfElements(arr, 10, 99)} элементов, удовлетворяющих условиям.");
