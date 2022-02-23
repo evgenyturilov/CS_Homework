f7357170cd20724f0f87b9875ef637e3d6aba1a7
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-23 23:44:39 +0300

    add code to Task_67

---
+++ Task_67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от N до 1, N задано.
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Series(int n)
+{
+    if (n == 1) return 1;
+    else return Series(n - 1) + 1;
+}
+
+
+Console.WriteLine($"Натуральные числа от {number} до 1:");
+for (int i = number; i >= 1; i--)
+{
+    Console.Write($"{Series(i)} ");
+}
