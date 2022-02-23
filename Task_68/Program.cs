4315c15d970c8fda7a4e820cf9dde640fd31ae82
Evgeny Turilov <evgenyturilov@gmail.com>
2022-02-23 23:56:49 +0300

    add code to Task_68

---
+++ Task_68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+Console.WriteLine("Введите первое число:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int Series(int n)
+{
+    if ( n == 1) return 1;
+    else return Series(n - 1) + 1;
+}
+
+Console.WriteLine($"Натуральные числа от {numberM} до {numberN}:");
+for (int i = numberM; i <= numberN; i++)
+{
+    Console.Write($"{Series(i)} ");
+}
