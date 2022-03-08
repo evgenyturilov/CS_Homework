602d235cb98dcb7a336a55b7dc1db3f5e636f1ed
Evgeny Turilov <evgenyturilov@gmail.com>
2022-03-08 23:06:14 +0300

    add code to Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+double a = 10, b = 4, c = 7;
+
+bool IsTriangle(double x, double y, double z)
+{
+    bool flag;
+    flag = (x + y > z && x + z > y && y + z > x);
+    return flag;
+}
+
+System.Console.WriteLine($"{a} {b} {c} - " + IsTriangle(a, b, c));
