30f141b1a9c2bdff1d26bb05dd2608f96f48f80c
Evgeny Turilov <evgenyturilov@gmail.com>
2022-03-13 20:14:15 +0300

    add code to Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определить номер четверти плоскости,
+// в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+System.Console.WriteLine("Введите координату X отличную от нуля:");
+int coordX = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y отличную от нуля:");
+int coordY = int.Parse(Console.ReadLine());
+
+if (coordX > 0 && coordY > 0) System.Console.WriteLine("Заданная точка находится в I четверти");
+if (coordX < 0 && coordY > 0) System.Console.WriteLine("Заданная точка находится во II четверти");
+if (coordX < 0 && coordY < 0) System.Console.WriteLine("Заданная точка находится в III четверти");
+if (coordX > 0 && coordY < 0) System.Console.WriteLine("Заданная точка находится в IV четверти");
+else System.Console.WriteLine("Одна из координат равна нулю");
--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -8,5 +8,7 @@ bool IsTriangle(double x, double y, double z)
     flag = (x + y > z && x + z > y && y + z > x);
     return flag;
 }
+// if -оператор
+// (условие) ? вернуть, если истино : вернуть, если ложно
 
-System.Console.WriteLine($"{a} {b} {c} - " + IsTriangle(a, b, c));
+System.Console.WriteLine($"{a} {b} {c} - " + (IsTriangle(a, b, c) ? "Это стороны треугольника" : "Этот не стороны треугольника"));
