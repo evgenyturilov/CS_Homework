613425c744ebb4b177789d5855e0247b281923dd
Evgeny Turilov <evgenyturilov@gmail.com>
2022-03-13 20:36:00 +0300

    add code to Task_20

---
+++ Task_20/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти (от 1 до 4):");
+int numberOfQuarter = int.Parse(Console.ReadLine());
+
+if (numberOfQuarter == 1) Console.WriteLine($"Максимальное значение координаты X = {Int32.MaxValue} , Y = {Int32.MaxValue}");
+if (numberOfQuarter == 2) Console.WriteLine($"Максимальное значение координаты X = {Int32.MinValue} , Y = {Int32.MaxValue}");
+if (numberOfQuarter == 3) Console.WriteLine($"Максимальное значение координаты X = {Int32.MinValue} , Y = {Int32.MinValue}");
+if (numberOfQuarter == 4) Console.WriteLine($"Максимальное значение координаты X = {Int32.MaxValue} , Y = {Int32.MinValue}");
