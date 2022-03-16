5360d6fa1e5c6bce8d39101528d2472d77c211c9
Evgeny <evgenyturilov@gmail.com>
2022-03-16 10:16:26 +0300

    add another way to solve Task_43

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Написать программу преобразования десятичного числа в двоичное
 
+
 System.Console.WriteLine("Введите число:");
 int decimalNumber = int.Parse(Console.ReadLine());
 
@@ -25,3 +26,23 @@ for (int i = len-1; i >= 0; i --)
 {
     Console.Write(binaryNumber[i]);
 }
+
+Console.WriteLine();
+
+// Решение №2 проще и использует конкатинацию строк, используем входные данные из решения 1 для сравнения результатов
+
+int temp = decimalNumber;
+int tempBinary = temp;
+string binaryNumber2 = " ";
+
+while (temp > 0)
+{
+    tempBinary = temp % 2;
+    binaryNumber2 = tempBinary.ToString() + binaryNumber2;
+    temp = temp/2;
+}
+
+Console.Write($"Десятичное число {decimalNumber} в двоичном виде: ");
+Console.WriteLine(binaryNumber2);
+
+Console.WriteLine();
