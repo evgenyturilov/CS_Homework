1a68a0ba08f1a2aea015c4e01f007d0fcc416bbc
Evgeny <evgenyturilov@gmail.com>
2022-03-17 10:03:35 +0300

    corrected code in Task_49, start to write comments in README

--- Task_49/Program.cs
+++ Task_49/Program.cs
@@ -1,25 +1,31 @@
 ﻿// Показать двумерный массив размером m×n заполненный вещественными числами
 
-int[,] matrix = new int[5,4];
+double[,] matrix = new double[5,4];
 
-void FillArray(int[,] matr)
+void FillArray(double[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++ )
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().Next(-9,10);
+            matr[i, j] = new Random().NextDouble()*100; // NextDouble генерирует вещественные числа
+            // matr[i,j] = Convert.ToDouble(new Random().Next(-99,100)) / 10;
+            // matr[i,j] = new Random().Next(-99,100) / 10D;
+            // matr[i,j] = new Random().Next(-99,100) / 10.1;
+            // matr[i,j] = double.Parse(new Random().Next(-99,100)) / 10;
+            // Console.Write("a[{0},{1}]={2,6:F2} ", i, j, a[i, j]); - F2 означает фиксированный вывод знаков после запятой (2 знака), можно использовать E2 - запись будет в экспоненциальном виде
+
         }
     }
 }
 
-void PrintArray(int[,] matr)
+void PrintArray(double[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            Console.Write($"  {matr[i,j]}  ");
+            Console.Write($" [{i},{j}]={matr[i,j],6:F2} ");
         }
         Console.WriteLine();
     }
