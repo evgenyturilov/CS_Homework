47f7ccc2afcf155f4cfde307e588714b1e635d13
Evgeny <evgenyturilov@gmail.com>
2022-03-25 15:07:31 +0300

    add code to Task_47

---
+++ Task_47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу копирования массива
+int[] originArray = {12,34,56,7,43,87};
+int[] array = new int[10];
+
+void FillArray(int[] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++ )
+    {
+        matr[i] = new Random().Next(0, 101);
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write($"{matr[i]} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+void CopyArray(int[] matr)
+
+{
+    Console.WriteLine("Массив копия:");
+    Console.WriteLine();
+    int[] copy = new int[matr.GetLength(0)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        copy[i] = matr[i];
+        Console.Write($"{copy[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//FillArray(array);
+//PrintArray(array);
+//CopyArray(array);
+
+CopyArray(originArray);
