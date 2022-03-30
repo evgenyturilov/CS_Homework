96ab40feb0bd6535fe82151184560df40f8f9939
Evgeny <evgenyturilov@gmail.com>
2022-03-30 12:52:01 +0300

    add code to Task_71

---
+++ Task_71/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0 && m > 0) return AkkermanFunction(m - 1, 1);
+    else return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+
+Console.WriteLine(AkkermanFunction(3,0));
