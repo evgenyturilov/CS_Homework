8cea9fb0f0f23440401c4124b1861a1e83e5db62
Evgeny <evgenyturilov@gmail.com>
2022-03-30 12:39:03 +0300

    add code to Task_72

---
+++ Task_72/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу возведения числа А в целую степень B
+
+int Rec(int a, int b)
+{
+    if (b == 1) return a;
+    else return a*Rec(a,b - 1);;
+}
+
+Console.WriteLine(Rec(3,6));
