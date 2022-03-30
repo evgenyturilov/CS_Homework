4cd070cea5c8d00033007257060ac8cdb3fe6c12
Evgeny <evgenyturilov@gmail.com>
2022-03-30 11:57:56 +0300

    add code to Task_70

---
+++ Task_70/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int number = 717;
+
+int SumOfDigits(int n)
+{
+    if (n < 10) return n;
+    else return n%10 + SumOfDigits(n/10);
+}
+
+Console.WriteLine(SumOfDigits(number));
