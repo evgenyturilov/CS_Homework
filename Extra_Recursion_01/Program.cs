04b80e5cc7dc3fa7f65e776a9f64130235dab699
Evgeny <evgenyturilov@gmail.com>
2022-03-31 10:09:41 +0300

    add code to Extra_Recursion_01

---
+++ Extra_Recursion_01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Является ли число результатом возведения 2 в натуральную степень
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+string IsExponentResult(int n)
+{
+    if (n == 1) return "Да";
+    else if (n%2 == 0) return IsExponentResult(n/2);
+    else return "Нет";
+}
+
+Console.WriteLine("Является ли число результатом возведения 2 в натуральную степень?");
+Console.WriteLine((IsExponentResult(number)));
